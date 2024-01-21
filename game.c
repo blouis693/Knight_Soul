778176ade1d3557d384ab1b9f20dd853f6382b33
brandon <blouis693@gmail.com>
2024-01-22 04:34:46 +0700

    added aurick and movements to aurick

--- game.c
+++ game.c
@@ -15,15 +15,13 @@ static ALLEGRO_DISPLAY* gameDisplay;
 static ALLEGRO_TIMER* gameTimer;
 static ALLEGRO_TIMER* gameTick;
 static ALLEGRO_EVENT_QUEUE* event_queue;
-extern int done;
+static ALLEGRO_EVENT event;
+static ALLEGRO_BITMAP* aurick = NULL;
+bool key[ALLEGRO_KEY_MAX] = { false };  // Array to track the state of each key
+
+int done = 0;
 //game loop
-void start_loop(void){
-    done = 0;
-    int redraw = 0;
-    while(!done){
-        
-    }
-}
+
 // Initialize Game Property
 
 void initGame(void){
@@ -48,6 +46,8 @@ void initGame(void){
         game_abort("failed to install keyboard");
     if (!al_install_mouse())
         game_abort("failed to install mouse");
+    if(!al_init_image_addon())
+        game_abort("couldn't initialize image addon\n");
     
     // Create Game Display
     gameDisplay = al_create_display(SCREEN_W, SCREEN_H);
@@ -66,7 +66,12 @@ void initGame(void){
 
     if (!gameTimer)
         game_abort("failed to create timer");
-  
+    // Create character
+    aurick = al_load_bitmap("pacman.png");
+    if(!aurick)
+    {
+        game_abort("cant load aurick");
+    }
     al_register_event_source(event_queue,
         al_get_display_event_source(gameDisplay));
     al_register_event_source(event_queue,
@@ -79,9 +84,76 @@ void initGame(void){
 
     al_start_timer(gameTimer);
 }
+void start_loop(void){
+    done = 0;
+    int redraw = 1;
+    float x = 0;
+    float y = 0;
+    float speed = 1;
+    while(!done){
+        al_wait_for_event(event_queue, &event);
 
+        switch(event.type)
+               {
+                   case ALLEGRO_EVENT_TIMER:
+                       // game logic goes here.
+                       redraw = 1;
+                       break;
+
+                   case ALLEGRO_EVENT_KEY_DOWN:
+                       key[event.keyboard.keycode] = true;
+
+                       break;
+                   case ALLEGRO_EVENT_KEY_UP:
+                       key[event.keyboard.keycode] = false;
+
+                       break;
+                   case ALLEGRO_EVENT_DISPLAY_CLOSE:
+                       done = 1;
+                       break;
+               }
+
+               if(done)
+                   break;
+              const float moveSpeed = 5.0;
+
+               if (key[ALLEGRO_KEY_W]) {
+                   y -= moveSpeed;
+               }
+               if (key[ALLEGRO_KEY_S]) {
+                   y += moveSpeed;
+               }
+               if (key[ALLEGRO_KEY_A]) {
+                   x -= moveSpeed;
+               }
+               if (key[ALLEGRO_KEY_D]) {
+                   x += moveSpeed;
+               }
+
+               if(redraw && al_is_event_queue_empty(event_queue))
+               {
+                   game_log("draw\n");
+                   al_clear_to_color(al_map_rgb(0, 0, 0));
+                   al_draw_bitmap(aurick,x,y, 0);
+//                   al_draw_bitmap(aurick,100,100, 0);
+//
+                   al_flip_display();
+                   redraw = 0;
+               }
+               
+    
+    }
+    al_destroy_display(gameDisplay);
+    al_destroy_timer(gameTimer);
+    al_destroy_event_queue(event_queue);
+    al_destroy_bitmap(aurick);
+
+}
 void startGame(void){
     initGame();
-    
+    game_log("game initialized");
+    start_loop();
     return;
 }
+
+
