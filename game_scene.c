1ff9759e8536f6f1b7843c70b009e116b25f9a77
brandon <blouis693@gmail.com>
2024-01-31 16:17:17 +0700

    added enemy

--- game_scene.c
+++ game_scene.c
@@ -1,21 +1,27 @@
 #include <string.h>
-
 #include "game_scene.h"
 #include "player.h"
 #include "map.h"
+#include "Enemy.h"
 #include <math.h>
-
+#include <time.h>
 Player player; // Player
 Map map; // Map
+Enemy enemy;
+int enemy_count = 0;
 extern int spawn_x;
 extern int spawn_y;
+enemy_list* enemy_group = NULL;
+enemy_list* tail_group = NULL;
+extern int TILE_SIZE;
+
 static void init(void){
     map = create_map("map0.txt", 0);
     
     
     player = create_player("pacman.png",spawn_y,spawn_x);
     game_log("coord x:%d \n coords y:%d \n",spawn_x,spawn_y);
-
+    enemy = create_enemy("pacman.png",1,1);
 }
 
 static void draw(void){
@@ -27,10 +33,28 @@ static void draw(void){
     // Draw
     draw_map(&map, Camera);
     draw_player(&player);
+    
+    draw_enemy(&enemy,Camera);
 }
 
 static void update(void){
+    Point dest = (Point){player.pos.x / TILE_SIZE,player.pos.y/TILE_SIZE};
+    enemy.coord = (Point){enemy.pos.x/TILE_SIZE,enemy.pos.y/TILE_SIZE};
     update_player(&player,&map);
+    update_enemy(&enemy, &map, enemy.coord,dest);
+//    if(enemy_count == 0){
+//        srand(time(0));
+//        int sx = Randoms(0, 19);
+//        int sy = Randoms(0,19);
+//        if(!enemy_group){
+//            enemy_group = insert_enemy(sx,sy);
+//            tail_group = enemy_group;
+//        }
+//        else{
+//            tail_group->next = insert_enemy(sx,sy);
+//            tail_group = tail_group->next;
+//        }
+//    }
 }
 
 static void destroy(void){
@@ -48,4 +72,7 @@ Scene create_game_scene(void){
     scene.destroy = &destroy;
     
     return scene;
+}
+int Randoms(int lower, int upper){
+    return (rand() % (upper - lower + 1)) + lower;
 }
--- game_scene.h
+++ game_scene.h
@@ -2,7 +2,9 @@
 #define game_scene_h
 
 #include "utility.h"
+#include "Enemy.h"
 
 Scene create_game_scene(void);
 
+int Randoms(int lower, int upper);
 #endif /* game_scene_h */
